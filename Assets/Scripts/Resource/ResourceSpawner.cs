83f7e063e734418dedd92a8addc954b71b0a7f3e
MonkinValery <monkin.vd@phystech.edu>
2021-07-10 21:33:40 +0300

    Inventory system

--- Assets/Scripts/Resource/ResourceSpawner.cs
+++ Assets/Scripts/Resource/ResourceSpawner.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class ResourceSpawner : MonoBehaviour
 {
-
     private Object _resourcePrefab;
+
+    private int _resourceBoxCounter = 0;
     private void Start()
     {
         Timer.spawnResource += CreateResource;
@@ -15,5 +16,11 @@ public class ResourceSpawner : MonoBehaviour
         GameObject resource = Instantiate(_resourcePrefab) as GameObject;
         resource.transform.SetParent(gameObject.transform, false);
         resource.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
+
+        resource.transform.position = new Vector3(resource.transform.position.x,
+                                                  _resourceBoxCounter * 1.2f,
+                                                  resource.transform.position.z);
+
+        _resourceBoxCounter += 1;
     }
 }
