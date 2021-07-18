bb37cc682627c056416669d3823aaccadb263764
MonkinValery <monkin.vd@phystech.edu>
2021-07-18 16:00:08 +0300

    Inventory

--- Assets/Scripts/Character/Inventory/Inventory.cs
+++ Assets/Scripts/Character/Inventory/Inventory.cs
@@ -2,20 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class Inventory : MonoBehaviour
+public class Inventory : MonoBehaviour, IPointerClickHandler
 {
-    [SerializeField] private InventoryPanel _inventoryPanel;
     [SerializeField] private int _maxSlots;
 
     private List<Item> _itemList;
 
-    public event Action OnItemListChanged;
+    public static event Action<Inventory> onCharacterClick;
 
-    private void Awake()
-    {
-        _inventoryPanel.SetInventory(this);
-    }
     private void Start()
     {
         _itemList = new List<Item>();
@@ -30,16 +27,20 @@ public class Inventory : MonoBehaviour
         if (chosenItems.Count() == 2)
         {
             int diffAmount = chosenItems.First().Amount.Increase(chosenItems.Last().Amount.GetAmount());
+            chosenItems.Last().Amount.Decrease(chosenItems.Last().Amount.GetAmount());
+            if(diffAmount > 0)
+            {
+                chosenItems.Last().Amount.Increase(diffAmount);
+            }
+            else
+            {
+                _itemList.Remove(chosenItems.Last());
+            }          
         }
         else if (chosenItems.Count() == 1)
         {
 
         }
-    }
-
-    private bool CanRemove(Item item)
-    {
-        return true;
     }
 
     public void AddItem(Item item)
@@ -56,13 +57,51 @@ public class Inventory : MonoBehaviour
         }
 
         PolishInventory(item);
-
-        OnItemListChanged.Invoke();
     }
 
+    public void RemoveItem(Item item)
+    {
+        if(item == null)
+        {
+            return;
+        }
+
+        var chosenItems =
+          from invItem in _itemList
+          where invItem.GetImage() == item.GetImage()
+          select invItem;
+
+        if(chosenItems.Count() >= 2)
+        {
+            int diffAmount = chosenItems.Last().Amount.Decrease(item.Amount.GetAmount());
+            if(diffAmount > 0)
+            {
+                _itemList.Remove(chosenItems.Last());
+                chosenItems.ToList().Remove(chosenItems.Last());
+
+                chosenItems.Last().Amount.Decrease(diffAmount);
+            }
+        }
+        else if(chosenItems.Count() == 1)
+        {
+            if(chosenItems.First().Amount.GetAmount() > item.Amount.GetAmount())
+            {
+                chosenItems.First().Amount.Decrease(item.Amount.GetAmount());
+            }
+            else if (chosenItems.First().Amount.GetAmount() == item.Amount.GetAmount())
+            {
+                _itemList.Remove(chosenItems.First());
+            }
+        }
+    }
 
     public List<Item> GetItemList()
     {
         return _itemList;
+    }
+
+    public void OnPointerClick(PointerEventData pointerEvent)
+    {
+        onCharacterClick.Invoke(this);       
     }
 }
